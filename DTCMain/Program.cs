115faca1b631a95812600ee3f03a7efacf64164b
DrZoid <drzoid@danstoncube.com>
2011-10-22 10:24:44 +0200

    Ajout DTCMain

---
+++ DTCMain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DTC
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //todo
+            Process p = new Process();
+            p.StartInfo.FileName = ".\\DTCUpdater.exe";
+            p.StartInfo.Arguments = "skipmain";
+            p.Start();
+        }
+    }
+}
